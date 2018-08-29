b69a53ecd89f1c444df6486666f9694c0806f091
MaryKattyVO <mkvicunaore@gmail.com>
2018-08-29 16:17:00 -0500

    cambios

--- cli.js
+++ cli.js
@@ -1,9 +1,49 @@
 #!/usr/bin/env node
-const [,, ...args] = process.argv;
-// console.log(`${args}`);
-
-const path = require('path').resolve;
 const fs = require('fs');
+const path = require('path').resolve;
 
-// console.log(path('/foo/bar', './baz'));
-console.log(path(`${args}`));
+const [,, ...args] = process.argv;
+
+
+//Definir si es carpeta o archivo
+    fs.stat(path(args[0]), (err, stats) => {
+        if (err) {
+            throw("Error");
+            
+        }
+        else {
+          if(stats.isDirectory()){
+          console.log(args[0] + "es carpeta");
+     
+     
+        }else if(stats.isFile()){
+          console.log(args[0] + "es archivo");
+     
+     
+        }else {
+          throw(args[0] + "error");
+          }
+        }
+    });
+
+// leer archivo de forma sincrona
+
+    fs.readFile(path(args[0]), 'utf-8', (err, data) => {
+      if(err) {
+        console.log('error: ', err);
+      } else {
+        console.log(data);
+      }
+    });
+
+    fs.readdir(path(args[0]), 'utf-8', (err, data) => {
+      if(err) {
+        console.log('error: ', err);
+      } else {
+        console.log(data);
+      }
+    });
+    // let archivo = fs.readFileSync('archivo2.txt', 'utf-8');
+    console.log(archivo);
+
+ 
---
+++ mat.js
@@ -0,0 +1,23 @@
+/*Primer ejemplo de modulo Math
+const Math = {};
+
+function add(x1, x2){
+    return x1 + x2;
+}
+function restar(x1, x2){
+    return x1 - x2;
+}
+ Math.add = add;
+ Math.restar = restar;
+
+ module.exports = Math;
+
+//  function hello (name){
+//      console.log('hola',name);
+//  }
+//  module.exports = hello;
+// exports.add = add;
+// exports.restar = restar;
+
+console.log(add(1,2));
+*/
